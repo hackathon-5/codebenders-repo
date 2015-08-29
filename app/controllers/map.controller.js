d8cef136749b5025fa2f94317946fa4219e6a5ce
JoelV <joel.villahermosa@gmail.com>
2015-08-29 08:16:24 -0400

    git push origin master -f

--- app/controllers/map.controller.js
+++ app/controllers/map.controller.js
@@ -4,6 +4,7 @@ var moment = require('moment');
 var rn = require('random-number');
 var gen = rn.generator({min: -10, max: 10 });
 var moment = require('moment');
+var uuid = require('uuid');
 
 module.exports = function($scope, $q, $modal, $templateCache, disasterService, DisasterTweets) {
 
@@ -66,44 +67,44 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
     }
   }
 
-  disasterService.getDisasters().then(function(res) {
-    console.log("res.data:", res.data);
-    $scope.markers = _.chain(res.data)
-      .filter(function(x) {
-        return moment(x.date).isAfter('2015-01-01');
-      })
-      .map(function(disaster, i) {
-        return {
-          id: i,
-          coords: {
-            latitude: parseInt(disaster.location.lat, 10) + gen(),
-            longitude: parseInt(disaster.location.long, 10) + gen()
-          },
-          options: {
-            dragable: false,
-            icon: {
-              url: getIcon(disaster.type)
-            }
-          },
-          data:  {
-            name: disaster.name,
-            type: getType(disaster.type),
-            description: disaster.description,
-            location: disaster.location.country,
-            date: disaster.date
-          }
-        }
-      })
-      .value();
+  // disasterService.getDisasters().then(function(res) {
+  //   console.log("res.data:", res.data);
+  //   $scope.markers = _.chain(res.data)
+  //     .filter(function(x) {
+  //       return moment(x.date).isAfter('2015-01-01');
+  //     })
+  //     .map(function(disaster, i) {
+  //       return {
+  //         id: i,
+  //         coords: {
+  //           latitude: parseInt(disaster.location.lat, 10) + gen(),
+  //           longitude: parseInt(disaster.location.long, 10) + gen()
+  //         },
+  //         options: {
+  //           dragable: false,
+  //           icon: {
+  //             url: getIcon(disaster.type)
+  //           }
+  //         },
+  //         data:  {
+  //           name: disaster.name,
+  //           type: getType(disaster.type),
+  //           description: disaster.description,
+  //           location: disaster.location.country,
+  //           date: disaster.date
+  //         }
+  //       }
+  //     })
+  //     .value();
 
-      // $scope.name = 'foo';
-      // $scope.type = 'flood';
-      // $scope.description = "Prolonged torrential rains caused a number of floods and mudslides between 11 and 13 May 2015 in Khatlon province and Hoit administrative center of the Rasht valley of Tajikistan. According to the rapid assessment results received from the Committee of Emergency Situations and Civil Defense and the Red Crescent Society of Tajikistan, 296 families (1,776 people) were severely affected. Most of the houses were heavily damaged and rendered unusable. Roads, bridges, schools, agricultural fields, family plots and four schools were also affected and destroyed. The affected population urgently needs shelter, food, hygiene and sanitation, drinking water and household appliances.";
-      // $scope.location = "USA";
-      // $scope.date = '08/23/2015';
-    console.log("$scope.markers:", $scope.markers);
+  //     // $scope.name = 'foo';
+  //     // $scope.type = 'flood';
+  //     // $scope.description = "Prolonged torrential rains caused a number of floods and mudslides between 11 and 13 May 2015 in Khatlon province and Hoit administrative center of the Rasht valley of Tajikistan. According to the rapid assessment results received from the Committee of Emergency Situations and Civil Defense and the Red Crescent Society of Tajikistan, 296 families (1,776 people) were severely affected. Most of the houses were heavily damaged and rendered unusable. Roads, bridges, schools, agricultural fields, family plots and four schools were also affected and destroyed. The affected population urgently needs shelter, food, hygiene and sanitation, drinking water and household appliances.";
+  //     // $scope.location = "USA";
+  //     // $scope.date = '08/23/2015';
+  //   console.log("$scope.markers:", $scope.markers);
 
-  });
+  // });
 
    // earthquake
    // http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Earthquakes-icon.png
@@ -130,78 +131,6 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
     })
   }
 
-  // need a way to fetch markers via api or whatevs
-
-
-
-
-   // $scope.markers = [{
-   //    // this can be html link
-   //    id: 0,
-   //    coords: {
-   //      // want to read these in from api service
-   //      latitude: 18.85,
-   //      longitude: -70.11
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        // fire
-   //        url: 'http://i572.photobucket.com/albums/ss164/sofidius/inside%20the%20fire/50px-Fire-icon-1.png'
-   //      },
-   //    }
-   //  },
-   //  {
-
-   //    id: 1,
-   //    coords: {
-   //      latitude: 42.1451,
-   //      longitude: -99.6680
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        // tornado
-   //        url: 'http://icons.iconarchive.com/icons/icons8/windows-8/24/Weather-Tornado-icon.png'
-   //      },
-   //    },
-   //  },
-   //  {
-   //    // this can be html link
-   //    id: 2,
-   //    coords: {
-   //      // want to read these in from api service
-   //      latitude: 32.1451,
-   //      longitude: -95.6680
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        // flood
-   //        url: 'http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Floods-icon.png'
-   //      },
-   //    },
-   //    title: 'foo'
-
-   //  },
-   //  {
-   //    // this can be html link
-   //    id: 3,
-   //    coords: {
-   //      // want to read these in from api service
-   //      latitude: 35.1451,
-   //      longitude: -99.6680
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        //earth quake
-   //        url: 'http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Earthquakes-icon.png'
-   //      },
-   //    },
-   //    title: 'foo'
-
-   //  },];
   $scope.getData = function () {
     $scope.data.events = [];
     $scope.data.tweets = [];
@@ -215,6 +144,8 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
         dataFetchers.push($scope.fetchDisasterTweets(d.users, d.type));
       }
     });
+
+    dataFetchers.push($scope.fetchDisasters());
 
     $q.all(dataFetchers).then( function () {
       $scope.data.tweets = _.sortBy($scope.data.tweets, 'created_at').reverse();
@@ -237,24 +168,68 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
           $scope.data.tweets.push(tweet);
         });
 
-        var events = _.map(response, function (res) {
-          return {
-            "name": angular.isDefined(res.entities) && angular.isDefined(res.entities.hashtags) && !angular.equals(res.entities.hashtags,[]) ? res.entities.hashtags[0].text : type,
-            "description": res.text,
-            "date": moment(res.created_at),
-            "type": type,
-            "location": {
-              "lat": angular.isDefined(res.coordinates) && angular.isDefined(res.coordinates.coordinates) ? res.coordinates.coordinates[1] : null,
-              "long": angular.isDefined(res.coordinates) && angular.isDefined(res.coordinates.coordinates) ? res.coordinates.coordinates[0] : null
-            }
-          };
+        var events = _.map($scope.data.tweets, function (res) {
+          if(res.coordinates) {
+            return {
+              id: uuid.v4(),
+              'coords': {
+                "latitude": res.coordinates.coordinates[1],
+                "longitude": res.coordinates.coordinates[0]
+              },
+              options: {
+                dragable: false,
+                icon: {
+                  url: getIcon({ code: type })
+                }
+              },
+              data:  {
+                name: angular.isDefined(res.entities) && angular.isDefined(res.entities.hashtags) && !angular.equals(res.entities.hashtags,[]) ? res.entities.hashtags[0].text : type,
+                type: type,
+                description: res.text,
+                location: 'Tempory Cat Country',
+                date: res.created_at 
+              }
+            };
+          }
         });
-        $scope.data.events.push(event);
-
+        $scope.data.events = $scope.data.events.concat(events);
+        $scope.data.events = _.compact($scope.data.events);
       }).catch(function (err) {
         console.log(err);
       });
   };
 
+  $scope.fetchDisasters = function() {
+    return disasterService.getDisasters().then(function(res) {
+      var disasters = _.chain(res.data)
+        .filter(function(x) {
+          return moment(x.date).isAfter('2014-01-01');
+        })
+        .map(function(disaster, i) {
+          return {
+            id: i,
+            coords: {
+              latitude: parseInt(disaster.location.lat, 10) + gen(),
+              longitude: parseInt(disaster.location.long, 10) + gen()
+            },
+            options: {
+              dragable: false,
+              icon: {
+                url: getIcon(disaster.type)
+              }
+            },
+            data:  {
+              name: disaster.name,
+              type: getType(disaster.type),
+              description: disaster.description,
+              location: disaster.location.country,
+              date: disaster.date
+            }
+          }
+        })
+        .value();
+      $scope.data.events = $scope.data.events.concat(disasters);
+    });
+  };
   $scope.getData();
 };
