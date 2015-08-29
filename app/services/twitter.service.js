9ad1eaf27d21e541dea61359531f675bb2071055
JoelV <joel.villahermosa@gmail.com>
2015-08-29 11:08:32 -0400

    crime crap

--- app/services/twitter.service.js
+++ app/services/twitter.service.js
@@ -6,6 +6,9 @@ module.exports = ['$http', '$q', function ($http, $q) {
       var params = query+"from:" + users.join(", OR from:");
 
       return $http.get('/api/getTweets', {params: {from: params}});
+    },
+    crimeTweets: function() {
+      return $http.get('/api/getCrimeTweets');
     }
   };
 }];
