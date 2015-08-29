a339e32a8f995ea62439651083090a4c5aaeaf7c
brentcobb <brent.tm.cobb@gmail.com>
2015-08-29 07:02:12 -0400

    added more types

--- app/controllers/map.controller.js
+++ app/controllers/map.controller.js
@@ -44,6 +44,10 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
       return 'http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Floods-icon.png';
     } else if (type.code === 'ST') {
       return 'http://icons.iconarchive.com/icons/icons8/windows-8/48/Weather-Storm-icon.png'
+    } else if (type.code === 'WF') {
+      return 'http://i572.photobucket.com/albums/ss164/sofidius/inside%20the%20fire/50px-Fire-icon-1.png';
+    } else if (type.code === 'EQ') {
+      return 'http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Earthquakes-icon.png'
     } else {
       return 'http://icons.iconarchive.com/icons/aha-soft/free-3d-glossy/48/Disaster-Bolt-icon.png'
     }
@@ -55,10 +59,14 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
       return 'Hurricane';
     } else if (type.code === 'VO') {
       return 'Volcano';
+    } else if (type.code === 'WF') {
+      return 'Wild Fire';
     } else if (type.code === 'CW') {
       return 'Cold Wave';
     } else if (type.code === ('FL' || 'FF')) {
       return 'Flood';
+    } else if (type.code === 'EQ') {
+      return "Earthquake"
     } else if (type.code === 'ST') {
       return 'Severe Storm'
     } else {
@@ -96,13 +104,6 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
       })
       .value();
 
-      // $scope.name = 'foo';
-      // $scope.type = 'flood';
-      // $scope.description = "Prolonged torrential rains caused a number of floods and mudslides between 11 and 13 May 2015 in Khatlon province and Hoit administrative center of the Rasht valley of Tajikistan. According to the rapid assessment results received from the Committee of Emergency Situations and Civil Defense and the Red Crescent Society of Tajikistan, 296 families (1,776 people) were severely affected. Most of the houses were heavily damaged and rendered unusable. Roads, bridges, schools, agricultural fields, family plots and four schools were also affected and destroyed. The affected population urgently needs shelter, food, hygiene and sanitation, drinking water and household appliances.";
-      // $scope.location = "USA";
-      // $scope.date = '08/23/2015';
-    console.log("$scope.markers:", $scope.markers);
-
   });
 
    // earthquake
@@ -122,6 +123,7 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
     $modal.open({
       controller: require('./markerModal.controller.js'),
       template: $templateCache.get('markerModal_view.html'),
+      size: 'lg',
       resolve: {
         disaster: function () {
           return marker;
@@ -130,78 +132,6 @@ module.exports = function($scope, $q, $modal, $templateCache, disasterService, D
     })
   }
 
-  // need a way to fetch markers via api or whatevs
-
-
-
-
-   // $scope.markers = [{
-   //    // this can be html link
-   //    id: 0,
-   //    coords: {
-   //      // want to read these in from api service
-   //      latitude: 18.85,
-   //      longitude: -70.11
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        // fire
-   //        url: 'http://i572.photobucket.com/albums/ss164/sofidius/inside%20the%20fire/50px-Fire-icon-1.png'
-   //      },
-   //    }
-   //  },
-   //  {
-
-   //    id: 1,
-   //    coords: {
-   //      latitude: 42.1451,
-   //      longitude: -99.6680
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        // tornado
-   //        url: 'http://icons.iconarchive.com/icons/icons8/windows-8/24/Weather-Tornado-icon.png'
-   //      },
-   //    },
-   //  },
-   //  {
-   //    // this can be html link
-   //    id: 2,
-   //    coords: {
-   //      // want to read these in from api service
-   //      latitude: 32.1451,
-   //      longitude: -95.6680
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        // flood
-   //        url: 'http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Floods-icon.png'
-   //      },
-   //    },
-   //    title: 'foo'
-
-   //  },
-   //  {
-   //    // this can be html link
-   //    id: 3,
-   //    coords: {
-   //      // want to read these in from api service
-   //      latitude: 35.1451,
-   //      longitude: -99.6680
-   //    },
-   //    options: {
-   //      draggable: false,
-   //      icon: {
-   //        //earth quake
-   //        url: 'http://icons.iconarchive.com/icons/icons8/ios7/24/Weather-Earthquakes-icon.png'
-   //      },
-   //    },
-   //    title: 'foo'
-
-   //  },];
   $scope.getData = function () {
     $scope.data.events = [];
     $scope.data.tweets = [];
